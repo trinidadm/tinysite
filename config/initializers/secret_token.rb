59e1741ad231854bb5fbd6e7d0961acb2e10114b
Martin Trinidad <wizzkidd@hotmail.com>
2013-07-28 01:45:48 -0700

    Created a rails application and added staus updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinysite::Application.config.secret_key_base = '41dedbaf91e662e2510c66be3b292f846cf4d24302829fccf92293ddcaa0b3e6f80d86a388b75bba657135fb836c1d2079a9c1ff88f8c7230a5dd558e3de9066'
